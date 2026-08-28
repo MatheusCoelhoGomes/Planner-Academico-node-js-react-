a8da1e1aae64051ac4a814c846ab39a1d8c91b84
Matheus Coelho Gomes <matheus16082006@gmail.com>
2026-08-28 14:47:14 -0300

    NavBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './NavBar.css'
-import logoUnirio from '../../assets/unirio.png'
 
 const NavBar = () => {
     return(
         <div className='navbar'>
-            <img src={logoUnirio} alt='logo' className='logo'/>
+            <h1>PLANNER ACADÊMICO</h1>
             <ul>
                 <li>Agenda</li>
-                <li>notas</li>
+                <li>Notas</li>
                 <li>Desempenho</li>
             </ul>
         </div>
---
+++ frontend/src/pages/Notas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './notas.css'
+
+const Notas = () =>{
+    return(
+        <div>
+            <h1>Notas</h1>
+        </div>
+    )
+}
+
+export default Notas
