d29554996d9af9e164dc79a4445d15462581b565
Matheus Coelho Gomes <matheus16082006@gmail.com>
2026-09-13 20:37:54 -0300

    .

--- frontend/src/components/Calendario/CriarData.jsx
+++ frontend/src/components/Calendario/CriarData.jsx
@@ -1,7 +1,6 @@
 import './CriarData.css'
 import BtnTexto from '../Buttons/Texto/BtnTexto'
-
-
+import BtnCircular from '../Buttons/Circular/BtnCircular'
 
 
 const FormDataTarefa = () => {
@@ -35,16 +34,21 @@ const CriarData = () => {
 
     return(
         <>
-            <BtnTexto margin ="2em 0em 0em 0em" onClick={() => {document.querySelector('.form-data').style.display = 'block';} }>Adicionar</BtnTexto>
+            <BtnTexto margin ="2em 0em 0em 0em" onClick={() => {document.querySelector('.form-data').style.display ="flex";} }>Adicionar</BtnTexto>
 
 
             <div className="form-data">
-                <div className="form-conteiner">
-                    
+                <div className='cabecalho'>
+                    <BtnCircular margin="1em 1em 0em 0em" onClick={() => {document.querySelector('.form-data').style.display ="none";}}></BtnCircular>
+                </div>
+                <div className="form-container">
+                    <div className='select'>
+                        <BtnTexto style={{borderRadius:'10px',}}>select</BtnTexto>
+                        <BtnTexto style={{borderRadius:'10px',margin:"0em 1em"}}>select</BtnTexto>
+                    </div>
                 </div>
             </div>
         </>
-
     )
 }
 
